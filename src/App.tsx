ef47f102bba1dead2c253a712cf17a559d7b8e93
João Sanson <joaosanson01@gmail.com>
2023-09-04 11:16:31 -0300

    feat: creating website structure :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
